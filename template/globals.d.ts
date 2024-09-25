1e0a0fac64d453585ec801fcf574389a5f8ed20b
Alex Sanchez <alexs@glue.digital>
2024-09-25 11:14:43 +0200

    add(globals): more types

--- template/globals.d.ts
+++ template/globals.d.ts
@@ -18,6 +18,16 @@ declare module '*.webp' {
   export default value
 }
 
+declare module '*.mp4' {
+  const value: string
+  export default value
+}
+
+declare module '*.webm' {
+  const value: string
+  export default value
+}
+
 declare module '*.css' {
   const content: { [className: string]: string }
   export default content
